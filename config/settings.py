42546590a3a307c3d3aa6ef7e0d083140413dd3f
adinaim <adinaitm@gmail.com>
2024-05-26 15:08:12 +0600

    errors fixed1

--- config/settings.py
+++ config/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
 
